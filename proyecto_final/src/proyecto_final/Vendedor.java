a08d674990246b538aa889b72a048864ef52c2f4
william sierra cordova <william.sierra@usil.pe>
2022-10-15 11:54:39 -0500

    Add files via upload

---
+++ proyecto_final/src/proyecto_final/Vendedor.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_final;
+
+/**
+ *
+ * @author WILLIAM
+ */
+public class Vendedor {
+    int codigo;
+    String nombre;
+    String dni;
+    String contraseña;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(int codigo, String nombre, String dni, String contraseña) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.contraseña = contraseña;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+}
